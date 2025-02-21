fefcec77504afd78c1fb8da5d9b2064a33e897d3
jogundas <jogundas@gmail.com>
2025-02-21 10:46:22 +0200

    add .env and papertrail

--- mnist.py
+++ mnist.py
@@ -1,5 +1,6 @@
 # /// script
 # dependencies = [
+#   "python-dotenv",
 #   "numpy",
 #   "wandb",
 #   "torch",
@@ -9,7 +10,6 @@
 # exclude-newer = "2024-02-20T00:00:00Z"
 # ///
 
-import itertools
 import random
 import torch
 import torch.nn as nn
@@ -25,6 +25,39 @@ import hashlib
 import os
 import math
 import socket
-from datetime import datetime
 from zoneinfo import ZoneInfo
 import wandb
+from dotenv import dotenv_values
+config = dotenv_values(".env")
+
+
+def create_papertrail_logger(config):
+    log_name = config.get("LOG_NAME", "MNIST")
+    timezone = config.get("TIMEZONE", "UTC")
+    papertrail_host = config.get("PAPERTRAIL_HOST")
+    papertrail_port = config.get("PAPERTRAIL_PORT")
+
+    def papertrail(message):
+        timestamp = datetime.now(ZoneInfo(timezone))
+        if papertrail_host and papertrail_port:
+            try:
+                with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+                    priority = 22
+                    hostname = ""
+                    syslog_message = (
+                        f"<{priority}>{timestamp.strftime("%b %d %H:%M:%S")} "
+                        f"{hostname} {log_name}: {message}"
+                    )
+                    sock.sendto(
+                        syslog_message.encode("utf-8"),
+                        (papertrail_host, int(papertrail_port)),
+                    )
+            except:
+                pass
+        print(f'{timestamp.strftime("%H:%M:%S")} {message}', flush=True)
+
+    return papertrail
+
+
+log = create_papertrail_logger(config)
+log("Test message")
